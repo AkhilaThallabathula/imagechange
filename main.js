4b8d35073b1ddce2700dc155fab598482529ab69
AkhilaThallabathula <146066134+AkhilaThallabathula@users.noreply.github.com>
2023-11-21 13:46:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeimg(){
+    document.getElementById("img1").src="pexels-photo-1933239.jpeg";
+    document.getElementById("img1").style.padding="10px";
+}
+function change(){
+    document.getElementById("img2").src="photo-1444703686981-a3abbc4d4fe3.jpeg";
+    document.getElementById("img2").style.padding="10px";
+}
+function changeimage1(){
+    document.getElementById("img3").src="a.jpg";
+    document.getElementById("img3").style.padding="10px";
+}
